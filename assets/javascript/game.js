ca032c817b7c79c235bb891dcc5d14ee24ad5603
InvNO1247 <62079147+InvNO1247@users.noreply.github.com>
2020-04-14 21:00:10 -0400

    starting on functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var letterUser = [];
-var eachletter = null; 
+var eachletter = null;
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+function countGuesses() {
+    document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + letterUser.join(" ");
+}
+
+countGuessesLeft();
+
+var restart = function() {
+    guessesLeft = 9;
+    letterUser = [];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
 
