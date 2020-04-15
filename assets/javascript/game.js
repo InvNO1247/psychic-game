de142de17d60e32a048a3a5e6e97b431937046e3
InvNO1247 <62079147+InvNO1247@users.noreply.github.com>
2020-04-15 14:06:14 -0400

    Error fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,50 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var letterUser = [];
-var eachletter = null;
+var eachofLetters = null;
+
+
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-function countGuesses() {
-    document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + letterUser.join(" ");
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
 }
 
 countGuessesLeft();
 
-var restart = function () {
-    guessesLeft = 9;
-    letterUser = [];
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
-document.onkeyup = function (event) {
-    guessesLeft--;
 
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+document.onkeyup = function(event) {
+	guessesLeft--;
 
-    letterUser.push(userGuess);
-    countGuessesLeft();
-    farUserGuesses();
-    
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    if (userGuess === computerGuess) {
-        wins++;
-        document.querySelector("#wins").innerHTML = "Wins: " + wins;
-        restart();
-    }
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
 
-    else if (guessesLeft ===) {
-        losses++;
-        document.querySelector("#lose").innerHTML = "Loses: " + losses;
-        restart();
-    }
-
-};
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
